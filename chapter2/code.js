f81cb3adee2804b944823833af7006d334deece7
Denisgby <denis.gby@gmail.com>
2021-10-27 22:01:45 +0300

    add random

--- chapter2/code.js
+++ chapter2/code.js
@@ -1,19 +1,28 @@
-var location1=3;
-var location2=4;
-var location3=5;
+var randomLoc = Math.floor(Math.random()*5)
+var location1=randomLoc;
+var location2=location1 + 1 ;
+var location3=location1 + 2 ;
 var guess;
 var hits=0;
 var guesses=0;
 var isSunk=false;
 
-while (isSunk==false){
-    guess=prompt('Ready, aim, fire! (enter a number 0-6):');
-    if (guess<0 || guess >6){
+while (isSunk == false) {
+    guess = prompt('Ready, aim, fire! (enter a number 0-6):');
+    if (guess < 0 || guess > 6) {
         window.alert('Please enter a valid cell number!');    
    } else {
-       guesses=guesses+1;
+       guesses = guesses + 1;
        if (guess == location1 || guess == location2 || guess == location3){
-           hits = hits + 1;
+            window.alert('HIT!');
+            hits = hits + 1;
+           if (hits == 3){
+               isSunk = true;
+               window.alert('You sank my battleship')
+           }
+       } else {
+           window.alert('MISS');
        }
    }
 }
+var stats = 'You took ' + guesses + ' guesses to sink the battleship, ' + ' which means your shooting accuracy was  ' + (3/guesses);
